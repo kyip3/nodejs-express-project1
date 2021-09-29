018908566e60dbebd5db0718840caebe06162d66
Yip Ka Yan <yipkayan3@gmail.com>
2021-09-29 22:33:58 +0800

    Completed
    Orders matters!

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.use('/users',(req,res,next)=>{
+    res.send("this is users page");
+});
+
+app.use('/',(req,res,next)=>{
+    res.send("hello word");
+});
 app.listen(3000);
